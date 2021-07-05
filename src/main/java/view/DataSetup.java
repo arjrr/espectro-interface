a93d0e8e95e07407950acd758b29d575431a2e47
Arilson <arilsonjr@outlook.com>
2021-07-05 07:50:01 -0300

    Adding maven

---
+++ src/main/java/view/DataSetup.java
@@ -0,0 +1,82 @@
+package view;
+
+import com.fazecast.jSerialComm.SerialPort;
+import util.Constants;
+
+import javax.swing.*;
+import java.util.Objects;
+
+public class DataSetup extends JFrame {
+
+    private static final String windowName = "Data Setup";
+    private static final int windowWidth = 800;
+    private static final int windowHeight = 400;
+
+    private JPanel dataSetupJPanel;
+    private JButton setPathButton;
+    private JLabel pathSelected;
+    private JComboBox<String> samplesComboBox;
+    private JComboBox<String> serialPortComboBox;
+    private JButton serialMonitorButton;
+    private JButton nextButton;
+    private JFileChooser jFileChooser;
+
+    public DataSetup() {
+        bindFrame();
+        setUpComponents();
+        setActionListeners();
+    }
+
+    private void setActionListeners() {
+        setPathButton.addActionListener(e -> {
+            jFileChooser = new JFileChooser();
+            jFileChooser.setFileSelectionMode(javax.swing.JFileChooser.DIRECTORIES_ONLY);
+            jFileChooser.showOpenDialog(this);
+            pathSelected.setText(jFileChooser.getSelectedFile().toString());
+        });
+
+        serialMonitorButton.addActionListener(e ->
+                new SerialMonitorDialog(SerialPort.getCommPort(Objects.requireNonNull(serialPortComboBox.getSelectedItem()).toString())).setVisible(true));
+
+        nextButton.addActionListener(e -> {
+            if (pathSelected.getText().equals("...")) {
+                showPathMessageDialog();
+            } else {
+                new DataRead(
+                        Objects.requireNonNull(samplesComboBox.getSelectedItem()).toString(),
+                        pathSelected.getText(),
+                        SerialPort.getCommPort(Objects.requireNonNull(serialPortComboBox.getSelectedItem()).toString())
+                ).setVisible(true);
+            }
+        });
+    }
+
+    private void bindFrame() {
+        setTitle(windowName);
+        setContentPane(dataSetupJPanel);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(windowWidth, windowHeight);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    private void setUpComponents() {
+        samplesComboBox.setModel(new DefaultComboBoxModel<>(Constants.Companion.getNumberOfSamples()));
+        getSerialPorts();
+    }
+
+    private void getSerialPorts() {
+        for (SerialPort port : SerialPort.getCommPorts()) {
+            serialPortComboBox.addItem(port.getSystemPortName());
+        }
+    }
+
+    private void showPathMessageDialog() {
+        JOptionPane.showMessageDialog(
+                this,
+                Constants.textPathDialog,
+                Constants.titlePathDialog,
+                JOptionPane.ERROR_MESSAGE);
+    }
+
+}
